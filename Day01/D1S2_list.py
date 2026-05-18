9a7c560a0c19608f0cb240ed9dd290b6eaa52483
gmaheswaranmca <gmaheswaranmca@gmail.com>
2026-05-18 13:39:02 +0530

    Py Learn D1 S2

---
+++ Day01/D1S2_list.py
@@ -0,0 +1,35 @@
+salaries = []
+
+salaries.append(250000) # to add a element to list 
+
+salaries.append(300000)
+
+salaries.append(800000)
+
+salaries.append(400000)
+
+salaries
+
+salaries
+
+first_salary = salaries[0] # index is read element using index number, list has zero based index
+
+hra_per = 20
+hra = (hra_per / 100) * first_salary
+
+hra
+
+salaries.pop() # delete last element 
+
+salaries
+
+salaries.pop(0) # delete element at position ie give index number 
+
+salaries
+
+salaries[0] = 325000
+
+salaries
+
+
+

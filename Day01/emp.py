15ed6dfa79d774340a684b3494007300fa64046a
gmaheswaranmca <gmaheswaranmca@gmail.com>
2026-05-18 11:03:00 +0530

    Py learn

---
+++ Day01/emp.py
@@ -0,0 +1,12 @@
+salary = 200000
+
+age = 47
+
+name = 'Maheswaran'
+
+is_active = True
+
+print('Name:', name)
+print('Age:', age)
+print('Salary:', salary)
+print('Status:', is_active)
---
+++ Day01/emp_input.py
@@ -0,0 +1,12 @@
+name = input('Name:')
+
+salary = float(input('Salary:'))
+
+age = int(input('Age:'))
+
+is_active = int(input('Status(1-Active, 2-Inactive):')) == 1
+
+print('Name:', name)
+print('Age:', age)
+print('Salary:', salary)
+print('Status:', is_active)
